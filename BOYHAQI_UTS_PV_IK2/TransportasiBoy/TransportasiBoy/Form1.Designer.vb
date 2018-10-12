8bf236006df7c3449d90a55f0e9390b62537827f
Unknown <boyhaqi888999@gmail.com>
2018-10-12 22:21:02 +0700

    UTSBoy
    uts

---
+++ BOYHAQI_UTS_PV_IK2/TransportasiBoy/TransportasiBoy/Form1.Designer.vb
@@ -0,0 +1,339 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TBnama = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TBidnumber = New System.Windows.Forms.TextBox()
+        Me.TBunit = New System.Windows.Forms.TextBox()
+        Me.TBunitD = New System.Windows.Forms.TextBox()
+        Me.TBareg = New System.Windows.Forms.TextBox()
+        Me.TBid = New System.Windows.Forms.TextBox()
+        Me.TBphone = New System.Windows.Forms.TextBox()
+        Me.TBintruksi = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.LVTranspor = New System.Windows.Forms.ListView()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DeleteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.BTSend = New System.Windows.Forms.Button()
+        Me.CBtranspo = New System.Windows.Forms.ComboBox()
+        Me.TBTranspppp = New System.Windows.Forms.TextBox()
+        Me.BTUpdate = New System.Windows.Forms.Button()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 77)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(99, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Name of Requestor"
+        '
+        'TBnama
+        '
+        Me.TBnama.Location = New System.Drawing.Point(131, 69)
+        Me.TBnama.Name = "TBnama"
+        Me.TBnama.Size = New System.Drawing.Size(136, 20)
+        Me.TBnama.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 115)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "ID Number"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(13, 153)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(26, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Unit"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(13, 191)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(70, 13)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Date of Issue"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(322, 76)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(96, 13)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "to Unit/Destination"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(322, 115)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(49, 13)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "A/C Reg"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(322, 153)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(75, 13)
+        Me.Label7.TabIndex = 7
+        Me.Label7.Text = "Transportation"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(322, 187)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(78, 13)
+        Me.Label8.TabIndex = 8
+        Me.Label8.Text = "Ext, Phone/Hp"
+        '
+        'TBidnumber
+        '
+        Me.TBidnumber.Location = New System.Drawing.Point(131, 108)
+        Me.TBidnumber.Name = "TBidnumber"
+        Me.TBidnumber.Size = New System.Drawing.Size(136, 20)
+        Me.TBidnumber.TabIndex = 9
+        '
+        'TBunit
+        '
+        Me.TBunit.Location = New System.Drawing.Point(131, 146)
+        Me.TBunit.Name = "TBunit"
+        Me.TBunit.Size = New System.Drawing.Size(136, 20)
+        Me.TBunit.TabIndex = 10
+        '
+        'TBunitD
+        '
+        Me.TBunitD.Location = New System.Drawing.Point(451, 69)
+        Me.TBunitD.Name = "TBunitD"
+        Me.TBunitD.Size = New System.Drawing.Size(121, 20)
+        Me.TBunitD.TabIndex = 12
+        '
+        'TBareg
+        '
+        Me.TBareg.Location = New System.Drawing.Point(451, 108)
+        Me.TBareg.Name = "TBareg"
+        Me.TBareg.Size = New System.Drawing.Size(121, 20)
+        Me.TBareg.TabIndex = 13
+        '
+        'TBid
+        '
+        Me.TBid.Location = New System.Drawing.Point(578, 69)
+        Me.TBid.Name = "TBid"
+        Me.TBid.Size = New System.Drawing.Size(100, 20)
+        Me.TBid.TabIndex = 14
+        '
+        'TBphone
+        '
+        Me.TBphone.Location = New System.Drawing.Point(451, 184)
+        Me.TBphone.Name = "TBphone"
+        Me.TBphone.Size = New System.Drawing.Size(121, 20)
+        Me.TBphone.TabIndex = 15
+        '
+        'TBintruksi
+        '
+        Me.TBintruksi.Location = New System.Drawing.Point(131, 264)
+        Me.TBintruksi.Multiline = True
+        Me.TBintruksi.Name = "TBintruksi"
+        Me.TBintruksi.Size = New System.Drawing.Size(441, 73)
+        Me.TBintruksi.TabIndex = 16
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(13, 264)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(56, 13)
+        Me.Label10.TabIndex = 18
+        Me.Label10.Text = "Intructions"
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.CustomFormat = "yyyy/MM/dd hh:mm:ss"
+        Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.DateTimePicker1.Location = New System.Drawing.Point(131, 184)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(136, 20)
+        Me.DateTimePicker1.TabIndex = 19
+        Me.DateTimePicker1.Value = New Date(2018, 10, 12, 19, 41, 26, 0)
+        '
+        'LVTranspor
+        '
+        Me.LVTranspor.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.LVTranspor.Location = New System.Drawing.Point(16, 368)
+        Me.LVTranspor.Name = "LVTranspor"
+        Me.LVTranspor.Size = New System.Drawing.Size(556, 135)
+        Me.LVTranspor.TabIndex = 20
+        Me.LVTranspor.UseCompatibleStateImageBehavior = False
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.EditToolStripMenuItem, Me.DeleteToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(108, 48)
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(107, 22)
+        Me.EditToolStripMenuItem.Text = "Edit"
+        '
+        'DeleteToolStripMenuItem
+        '
+        Me.DeleteToolStripMenuItem.Name = "DeleteToolStripMenuItem"
+        Me.DeleteToolStripMenuItem.Size = New System.Drawing.Size(107, 22)
+        Me.DeleteToolStripMenuItem.Text = "Delete"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(173, 25)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(245, 20)
+        Me.Label9.TabIndex = 21
+        Me.Label9.Text = "Transportation Request Form"
+        '
+        'BTSend
+        '
+        Me.BTSend.Location = New System.Drawing.Point(13, 525)
+        Me.BTSend.Name = "BTSend"
+        Me.BTSend.Size = New System.Drawing.Size(99, 23)
+        Me.BTSend.TabIndex = 22
+        Me.BTSend.Text = "Send Request"
+        Me.BTSend.UseVisualStyleBackColor = True
+        '
+        'CBtranspo
+        '
+        Me.CBtranspo.FormattingEnabled = True
+        Me.CBtranspo.Items.AddRange(New Object() {"Pick Up", "Box", "Blind Van", "Truck", "BTT", "Forklift", "Other"})
+        Me.CBtranspo.Location = New System.Drawing.Point(451, 144)
+        Me.CBtranspo.Name = "CBtranspo"
+        Me.CBtranspo.Size = New System.Drawing.Size(121, 21)
+        Me.CBtranspo.TabIndex = 23
+        '
+        'TBTranspppp
+        '
+        Me.TBTranspppp.Location = New System.Drawing.Point(578, 145)
+        Me.TBTranspppp.Name = "TBTranspppp"
+        Me.TBTranspppp.Size = New System.Drawing.Size(100, 20)
+        Me.TBTranspppp.TabIndex = 24
+        '
+        'BTUpdate
+        '
+        Me.BTUpdate.Location = New System.Drawing.Point(13, 525)
+        Me.BTUpdate.Name = "BTUpdate"
+        Me.BTUpdate.Size = New System.Drawing.Size(99, 23)
+        Me.BTUpdate.TabIndex = 26
+        Me.BTUpdate.Text = "Update"
+        Me.BTUpdate.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(704, 569)
+        Me.Controls.Add(Me.BTUpdate)
+        Me.Controls.Add(Me.TBTranspppp)
+        Me.Controls.Add(Me.CBtranspo)
+        Me.Controls.Add(Me.BTSend)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.LVTranspor)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.TBintruksi)
+        Me.Controls.Add(Me.TBphone)
+        Me.Controls.Add(Me.TBid)
+        Me.Controls.Add(Me.TBareg)
+        Me.Controls.Add(Me.TBunitD)
+        Me.Controls.Add(Me.TBunit)
+        Me.Controls.Add(Me.TBidnumber)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TBnama)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TBnama As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents TBidnumber As System.Windows.Forms.TextBox
+    Friend WithEvents TBunit As System.Windows.Forms.TextBox
+    Friend WithEvents TBunitD As System.Windows.Forms.TextBox
+    Friend WithEvents TBareg As System.Windows.Forms.TextBox
+    Friend WithEvents TBid As System.Windows.Forms.TextBox
+    Friend WithEvents TBphone As System.Windows.Forms.TextBox
+    Friend WithEvents TBintruksi As System.Windows.Forms.TextBox
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents DateTimePicker1 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents LVTranspor As System.Windows.Forms.ListView
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents BTSend As System.Windows.Forms.Button
+    Friend WithEvents CBtranspo As System.Windows.Forms.ComboBox
+    Friend WithEvents TBTranspppp As System.Windows.Forms.TextBox
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents EditToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents DeleteToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents BTUpdate As System.Windows.Forms.Button
+
+End Class
---
+++ BOYHAQI_UTS_PV_IK2/TransportasiBoy/TransportasiBoy/Form1.vb
@@ -0,0 +1,180 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Call OpenKoneksi()
+        Dim cmd As SqlCommand
+        Dim dr As SqlDataReader
+        Dim strsql As String
+        TBid.Visible = False
+        TBTranspppp.Visible = False
+        BTUpdate.Visible = False
+
+
+        Try
+            strsql = "select * from Transportasi"
+            cmd = New SqlCommand(strsql, conn)
+            dr = cmd.ExecuteReader
+            dr.Close()
+            Call CloseKoneksi()
+            cler_from()
+            LVTranspor.Clear()
+            show_list_Transportasi()
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+
+        End Try
+    End Sub
+    Private Sub cler_from()
+        TBnama.Text = ""
+        TBidnumber.Text = ""
+        TBunit.Text = ""
+        DateTimePicker1.Value = Now
+        TBunitD.Text = ""
+        TBareg.Text = ""
+        CBtranspo.Text = ""
+        TBphone.Text = ""
+        TBintruksi.Text = ""
+        TBTranspppp.Text = ""
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BTSend.Click
+        Dim query As String
+        Dim cmd As SqlCommand
+        Dim rd As SqlDataReader
+        Dim uid As String
+
+
+
+
+        Try
+
+
+            Call OpenKoneksi()
+            Dim query_id As String = "SELECT MAX(RequestId)+1 AS MXID FROM Transportasi"
+            cmd = New SqlCommand(query_id, conn)
+            rd = cmd.ExecuteReader
+            rd.Read()
+            uid = rd("MXID")
+            rd.Close()
+
+
+
+            query = "INSERT INTO Transportasi (RequestId,NameRequestor,RequestorId,Unit,DateIssue,UnitDestination,acRegis,Transportation,Phone,Intruction) VALUES (" & uid & ",'" & TBnama.Text & "','" & TBidnumber.Text & "', '" & TBunit.Text & "','" & DateTimePicker1.Value & "','" & TBunitD.Text & "','" & TBareg.Text & "','" & CBtranspo.Text & "','" & TBphone.Text & "','" & TBintruksi.Text & "')"
+
+
+
+            cmd = New SqlCommand(query, conn)
+            rd = cmd.ExecuteReader
+            rd.Close()
+
+            cler_from()
+            LVTranspor.Clear()
+            show_list_Transportasi()
+
+
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub show_list_Transportasi()
+        Dim cmd As SqlCommand        Dim dr As SqlDataReader        LVTranspor.View = View.Details        LVTranspor.Columns.Add("NO", 50)        LVTranspor.Columns.Add("Name Of Requestor", 125)        LVTranspor.Columns.Add("Unit From", 90)        LVTranspor.Columns.Add("Unit To", 70)        LVTranspor.Columns.Add("Request Date", 100)        LVTranspor.Columns.Add("Transportation", 100)        Call OpenKoneksi()        Dim query_id As String = "select * from Transportasi"        cmd = New SqlCommand(query_id, conn)        dr = cmd.ExecuteReader        Do While dr.Read            LVTranspor.Items.Add(dr("RequestId"))            LVTranspor.Items(LVTranspor.Items.Count - 1).SubItems.Add(dr("NameRequestor"))            LVTranspor.Items(LVTranspor.Items.Count - 1).SubItems.Add(dr("Unit"))            LVTranspor.Items(LVTranspor.Items.Count - 1).SubItems.Add(dr("UnitDestination"))            LVTranspor.Items(LVTranspor.Items.Count - 1).SubItems.Add(dr("DateIssue"))            LVTranspor.Items(LVTranspor.Items.Count - 1).SubItems.Add(dr("Transportation"))        Loop        dr.Close()        Call CloseKoneksi()
+    End Sub
+
+    Private Sub EditToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles EditToolStripMenuItem.Click
+        Dim cmd As SqlCommand        Dim dr As SqlDataReader
+        Dim IDR As String
+        BTUpdate.Visible = True
+        BTSend.Visible = False
+
+        Try
+            IDR = LVTranspor.SelectedItems(0).Text
+            Call OpenKoneksi()
+            Dim query As String = "SELECT * FROM Transportasi WHERE RequestId='" + IDR + "'"            cmd = New SqlCommand(query, conn)            dr = cmd.ExecuteReader
+
+            dr.Read()
+            TBid.Text = dr("RequestId")
+            TBnama.Text = dr("NameRequestor")
+            TBidnumber.Text = dr("RequestorId")
+            TBunit.Text = dr("Unit")
+            DateTimePicker1.Value = Now
+            TBunitD.Text = dr("UnitDestination")
+            TBareg.Text = dr("acRegis")
+            CBtranspo.Text = dr("Transportation")
+            TBphone.Text = dr("Phone")
+            TBintruksi.Text = dr("Intruction")
+
+            dr.Close()
+            Call CloseKoneksi()
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub DeleteToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles DeleteToolStripMenuItem.Click
+        Dim Idr As String
+        Dim cmd As SqlCommand        Dim dr As SqlDataReader
+
+        Try
+            Idr = LVTranspor.SelectedItems(0).Text
+
+            Call OpenKoneksi()
+            Dim query As String = "delete from Transportasi where RequestId='" + Idr + "'"            cmd = New SqlCommand(query, conn)            dr = cmd.ExecuteReader
+            Call CloseKoneksi()
+            dr.Close()
+
+            LVTranspor.Clear()
+            show_list_Transportasi()
+        Catch ex As Exception
+            MsgBox(ex.ToString())
+        End Try
+    End Sub
+
+    Private Sub BTUpdate_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BTUpdate.Click
+        Dim query As String
+        Dim cmd As SqlCommand
+        Dim dr As SqlDataReader
+        Dim uid As String
+
+        BTUpdate.Visible = False
+        BTSend.Visible = True
+        Try
+            Call OpenKoneksi()
+            Dim query_id As String = "SELECT MAX(RequestId)+1 AS MXID FROM Transportasi"
+            cmd = New SqlCommand(query_id, conn)
+            dr = cmd.ExecuteReader
+            dr.Read()
+            uid = dr("MXID")
+            dr.Close()
+
+            query = " Update Transportasi SET NameRequestor='" & TBnama.Text & "',RequestorId='" & TBidnumber.Text & "',Unit='" & TBunit.Text & "',DateIssue='" & DateTimePicker1.Value & "',UnitDestination='" & TBunitD.Text & "',acRegis='" & TBareg.Text & "',Transportation='" & CBtranspo.Text & "',Phone='" & TBphone.Text & "',Intruction='" & TBintruksi.Text & "' where RequestId='" & TBid.Text & "'"
+
+            cmd = New SqlCommand(query, conn)
+            dr = cmd.ExecuteReader
+            dr.Close()
+
+            Call CloseKoneksi()
+            cler_from()
+            LVTranspor.Clear()
+            show_list_Transportasi()
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+    Private Sub CBtranspo_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CBtranspo.SelectedIndexChanged
+        If CBtranspo.Text = "Other" Then
+            TBTranspppp.Visible = True
+        End If
+    End Sub
+
+    Private Sub TBTranspppp_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TBTranspppp.TextChanged
+        CBtranspo.Text = TBTranspppp.Text
+
+
+        CBtranspo.Enabled = False
+
+
+    End Sub
+End Class
